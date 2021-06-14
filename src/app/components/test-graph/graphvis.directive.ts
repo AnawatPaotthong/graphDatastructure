c3087c0fa7352231892670307feb1876e1e10301
Anawat Paothong <anawat.paothong@mail.kmutt.ac.th>
2021-06-14 12:15:49 +0700

    align the nodes and make code read easier

--- src/app/components/test-graph/graphvis.directive.ts
+++ src/app/components/test-graph/graphvis.directive.ts
@@ -33,18 +33,100 @@ export class GraphVisDirective {
       },
       groups: {
         mainNodes:{
-          color: { border: '#ff0000', background: '#ffcccc', highlight: { border: '#cc0088', background: '#ffcccc' } }, font: { color: '#660066', size: 28, face: 'TH SarabunPSK' }, margin:{ top: 15, right: 15, left: 15, bottom: 15}
+          color: {
+            border: '#ff0000',
+            background: '#ffcccc',
+            highlight: { 
+            border: '#cc0088',
+            background: '#ffcccc' 
+            } 
+          },
+          font: {
+            color: '#660066',
+            size: 28,
+            face: 'TH SarabunPSK' 
+          },
+          margin: {
+            top: 15,
+            right: 15,
+            left: 15,
+            bottom: 15
+          }
         },
         secondaryNodes:{
-          color: { border: '#ff0000', background: '#ffd1b3', highlight: { border: '#ff0000', background: '#ffd1b3' } }, font: { color: '#331400', size: 20, face: 'TH SarabunPSK' }
+          color: {
+            border: '#ff0000',
+            background: '#ffd1b3',
+            highlight: {
+              border: '#ff0000',
+              background: '#ffd1b3'
+            } 
+          }, 
+          font: {
+            color: '#331400',
+            size: 20,
+            face: 'TH SarabunPSK' 
+          }
         },
         secondaryNodes1:{
-          color: { border: '#ff0000', background: '#00cc00', highlight: { border: '#ff0000', background: '#00cc00' } }, font: { color: '#000000', size: 20, face: 'TH SarabunPSK' }
+          color: { 
+            border: '#ff0000',
+            background: '#00cc00',
+            highlight: {
+              border: '#ff0000',
+              background: '#00cc00'
+            }
+          },
+          font: {
+            color: '#000000',
+            size: 20,
+            face: 'TH SarabunPSK'
+          }
         },
         secondaryNodes2:{
-          color: { border: '#ff0000', background: '#c2f0c2', highlight: { border: '#ff0000', background: '#c2f0c2' } }, font: { color: '#051405', size: 20, face: 'TH SarabunPSK' }
+          color: {
+            border: '#ff0000',
+            background: '#c2f0c2',
+            highlight: {
+              border: '#ff0000',
+              background: '#c2f0c2' 
+            }
+          },
+          font: {
+            color: '#051405',
+            size: 20,
+            face: 'TH SarabunPSK' }
+        },
+        secondaryNodes3:{
+          color: {
+            border: '#ff0000',
+            background: '#ffffb3',
+            highlight: {
+              border: '#ff0000',
+              background: '#ffffb3'
+            }
+          },
+          font: {
+            color: '#cc0000',
+            size: 20,
+            face: 'TH SarabunPSK'
+          }
+        },
+        bottomNodes:{
+          color: {
+            border: '#0000FF',
+            background: '#f0f0f5',
+            highlight: {
+              border: '#0000FF',
+              background: '#f0f0f5' }
+            },
+            font: {
+              color: '#0033cc',
+              size: 20,
+              face: 'TH SarabunPSK'
+            }
         }
-      },
+      }
     };
 
     if(!this.network){
